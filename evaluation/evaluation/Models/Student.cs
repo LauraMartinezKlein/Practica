f84da114d4b421f5c5e45da909d9d9916524a45d
visual_community@outlook.com <visual_community@outlook.com>
2019-09-27 16:45:12 -0400

    ·er commit

---
+++ evaluation/evaluation/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace evaluation.Models
+{
+    public enum TypeGender {
+        Male,
+        Female
+    }
+
+    public class Student
+    {
+        [Key]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Birthdate { get; set; }
+
+
+        [Required]
+        public TypeGender Gender { get; set; }
+
+    }
+
+}
